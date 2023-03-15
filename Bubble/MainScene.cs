62ca2a974817659ca06c31dd29d2add23324ae45
0skr0 <pech47pechsakkarin@gmail.com>
2023-03-15 14:30:54 +0700

    as

--- Bubble/MainScene.cs
+++ Bubble/MainScene.cs
@@ -17,7 +17,7 @@ namespace Bubble
         public int[,] _bbPos;
 
         public Color[] allColor;
-
+        //sa
         public MainScene()
         {
             _graphics = new GraphicsDeviceManager(this);
