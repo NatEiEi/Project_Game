b0ca7f77786c273102dca816205a245f37a2df75
0skr0 <pech47pechsakkarin@gmail.com>
2023-03-15 18:20:53 +0700

    add Singleton

--- Bubble/MainScene.cs
+++ Bubble/MainScene.cs
@@ -10,17 +10,9 @@ namespace Bubble
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Random rnd = new Random();
+        public Texture2D _circle, _bubble, _rect, _bg;
 
-        public Texture2D _circle, _bubble, _rect;
-
-        public int[,] _bbPos;
-
-        public Color[] allColor;
-
-        public int _bbHeight, _tileSize;
-
-        public KeyboardState _previousKey, _currentKey;
+        public int _bbHeight;
 
         public MainScene()
         {
@@ -32,14 +24,11 @@ namespace Bubble
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            _graphics.PreferredBackBufferWidth = 1200;
-            _graphics.PreferredBackBufferHeight = 900;
+            _graphics.PreferredBackBufferWidth = Singleton.ScreenWidth;
+            _graphics.PreferredBackBufferHeight = Singleton.ScreenHeight;
             _graphics.ApplyChanges();
 
-            allColor = new Color[6];
-            _bbPos = new int[11,10];
-            _bbHeight = 5;
-            _tileSize = 60;
+            Reset();
 
             base.Initialize();
         }
@@ -49,6 +38,7 @@ namespace Bubble
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _bg = this.Content.Load<Texture2D>("BG_Bubble");
             _bubble = this.Content.Load<Texture2D>("bubble");
             _circle = this.Content.Load<Texture2D>("circle");
             _rect = new Texture2D(_graphics.GraphicsDevice, 1, 1);
@@ -57,21 +47,6 @@ namespace Bubble
             data[0] =Color.White;
             _rect.SetData(data);
 
-            allColor[0] = Color.Red;
-            allColor[1] = Color.Green;
-            allColor[2] = Color.Blue;
-            allColor[3] = Color.Yellow;
-            allColor[4] = Color.Orange;
-            allColor[5] = Color.Purple;
-
-            for(int i = 0; i < 11; i++)
-            {
-                for(int j = 0; j < 10; j++)
-                {
-                    _bbPos[i, j] = rnd.Next(allColor.Length);
-                    if (i > 5) _bbPos[i, j] = -1;
-                }
-            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -80,14 +55,14 @@ namespace Bubble
                 Exit();
 
             // TODO: Add your update logic here
-            _currentKey = Keyboard.GetState();
+            Singleton.Instance._currentKey = Keyboard.GetState();
 
-            if (_currentKey.IsKeyDown(Keys.Space) && !_currentKey.Equals(_previousKey))
+            if (Singleton.Instance._currentKey.IsKeyDown(Keys.Space) && !Singleton.Instance._currentKey.Equals(Singleton.Instance._previousKey))
             {
-                _bbHeight += _tileSize;
+                _bbHeight += Singleton.TileSize;
             }
 
-            _previousKey = _currentKey;
+            Singleton.Instance._previousKey = Singleton.Instance._currentKey;
 
 
             base.Update(gameTime);
@@ -95,42 +70,42 @@ namespace Bubble
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.Aqua);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(_rect, Vector2.Zero, null, Color.DarkGoldenrod, 0f, Vector2.Zero, new Vector2(300 - 15, 900), SpriteEffects.None, 0f);
-            _spriteBatch.Draw(_rect, new Vector2(900+15, 0), null, Color.DarkGoldenrod, 0f, Vector2.Zero, new Vector2(300 - 15, 900), SpriteEffects.None, 0f);
-
-            _spriteBatch.Draw(_rect, new Vector2(300-15, 5+(10*60)), null, Color.DarkRed, 0f, Vector2.Zero, new Vector2(630, 2), SpriteEffects.None, 0f);
+            _spriteBatch.Draw(_bg, Vector2.Zero, Color.White);
 
             for (int i = 0; i < 11; i++) 
             { 
                 for(int j = 0; j < 10; j++)
                 {
-                    if (_bbPos[i,j] != -1)
+                    if (Singleton.Instance._bbColor[i,j] != -1)
                     {
                         if(i % 2 == 0)
                         {
-                            _spriteBatch.Draw(_circle, new Vector2(300 + (j * 60) -15, _bbHeight + (i * 60)), allColor[_bbPos[i, j]]);
-                            _spriteBatch.Draw(_bubble, new Vector2(300 + (j * 60) -15, _bbHeight + (i * 60)), Color.White);
+                            _spriteBatch.Draw(_circle, new Vector2(Singleton.LeftMargin + (j * Singleton.TileSize), _bbHeight + (i * Singleton.TileSize)), Singleton.Instance.allColor[Singleton.Instance._bbColor[i, j]]);
+                            _spriteBatch.Draw(_bubble, new Vector2(Singleton.LeftMargin + (j * Singleton.TileSize), _bbHeight + (i * Singleton.TileSize)), Color.LightCyan);
                         }
                         else
                         {
-                            _spriteBatch.Draw(_circle, new Vector2(300 + (j * 60) +15, _bbHeight + (i * 60)), allColor[_bbPos[i, j]]);
-                            _spriteBatch.Draw(_bubble, new Vector2(300 + (j * 60) +15, _bbHeight + (i * 60)), Color.White);
+                            _spriteBatch.Draw(_circle, new Vector2(Singleton.LeftMargin + (Singleton.TileSize/2) + (j * Singleton.TileSize), _bbHeight + (i * Singleton.TileSize)), Singleton.Instance.allColor[Singleton.Instance._bbColor[i, j]]);
+                            _spriteBatch.Draw(_bubble, new Vector2(Singleton.LeftMargin + (Singleton.TileSize/2) + (j * Singleton.TileSize), _bbHeight + (i * Singleton.TileSize)), Color.LightCyan);
                         }
                     }
                 }
             }
 
-
-
             _spriteBatch.End();
             _graphics.BeginDraw();
 
             base.Draw(gameTime);
+        }
+
+        protected void Reset()
+        {
+            _bbHeight = 5;
         }
     }
 }
---
+++ Bubble/Singleton.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble
+{
+    class Singleton
+    {
+        private static Singleton instance;
+
+        public const int TileSize = 60;
+        public const int ScreenWidth = 1200;
+        public const int ScreenHeight = 900;
+
+        public const int LeftMargin = 285;
+        public const int RightMargin = LeftMargin + 630;
+        public const int RedLine = (TileSize * 10) + 5;
+
+        public KeyboardState _previousKey, _currentKey;
+
+        public Random rnd = new Random();
+
+        public Color[] allColor = {Color.Red, Color.Green, Color.Blue, Color.Yellow};
+        public int[,] _bbColor = { { 0, 1, 2, 3, 1, 2, 3, 2, 3, 3 }
+                                , { 0, 0, 3, 0, 3, 2, 0 ,3, 2, 1}
+                                , { 2, 3, 0, 1, 3, 0, 1, 0, 1, 1}
+                                , { 2, 2, 1, 2, 1, 0, 2, 1, 0, 3}
+                                , { 0, 3, 1, 2, 0, 3, 1, 0, 3, 0}
+                                , { 1, 2, 0, 3, 1, 2, 0, 1, 2, 1} 
+                                , { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+                                , { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+                                , { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+                                , { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
+                                , { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1} };
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get 
+            { 
+                if(instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
