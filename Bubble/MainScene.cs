2cbcbf82c4b547dfbbcd832b3afbd9d7fbe34a0f
nat nat <natthaphonlove2545@gmail.com>
2023-03-15 19:56:29 +0700

    Shooting

--- Bubble/MainScene.cs
+++ Bubble/MainScene.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Bubble
 {
@@ -13,14 +14,36 @@ namespace Bubble
         public Random rnd = new Random();
 
         public Texture2D _circle, _bubble, _rect , _Trident;
-
+        SpriteFont _font;
         public int[,] _bbPos;
 
         public Color[] allColor;
 
-        MouseState _mouseState;
+        MouseState _mouseState , _previousMouseState;
         Point TridentPos ;
-        float _rotateAngle;
+        
+        float _rotateAngle , _moveAngle;
+        Vector2 relPoint;
+        float _tick;
+        //Point _move;
+        float _moveSpeed , _PlusX , _PlusY , _moveX , _moveY;
+        KeyboardState _currentKey, _previousKey;
+
+
+
+        Vector2 _CurrentBubblePos;
+        int _CurrentBubble , _NextBubble;
+        GameState _currentGameState;
+
+        enum GameState
+        {
+            GameStarted,
+            GamePaused,
+            WaitingForShooting,
+            Shoot,
+            GameEnded
+        }
+
 
         public MainScene()
         {
@@ -38,7 +61,10 @@ namespace Bubble
 
             allColor = new Color[6];
             _bbPos = new int[11,10];
-            TridentPos = new Point(600, 860);
+            TridentPos = new Point(600, 880);
+
+            _currentGameState = GameState.WaitingForShooting;
+            Reset();
 
             base.Initialize();
         }
@@ -52,7 +78,7 @@ namespace Bubble
             _circle = this.Content.Load<Texture2D>("circle");
             _Trident = this.Content.Load<Texture2D>("Trident");
             _rect = new Texture2D(_graphics.GraphicsDevice, 1, 1);
-
+            _font = this.Content.Load<SpriteFont>("GameFont");
             Color[] data = new Color[1];
             data[0] =Color.White;
             _rect.SetData(data);
@@ -73,14 +99,82 @@ namespace Bubble
                 }
             }
         }
-
+        
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-            _mouseState = Mouse.GetState();
+            _previousMouseState = _mouseState;
+
+
+
+            switch (_currentGameState)
+            {
+                case GameState.GameStarted:
+                    break;
+                case GameState.GamePaused:
+                    break;
+                case GameState.WaitingForShooting:
+
+                    _mouseState = Mouse.GetState();
+                    //Trident Move
+                    relPoint = new Vector2((_mouseState.X - TridentPos.X) ,(_mouseState.Y - TridentPos.Y)) ;
+                    _rotateAngle = (MathHelper.ToDegrees(MathF.Atan2(relPoint.Y, relPoint.X)) + 450f) % 360f;
+                    _rotateAngle = MathHelper.ToRadians(_rotateAngle);
+
+
+
+                    if (_mouseState.LeftButton == ButtonState.Pressed &&
+                        _previousMouseState.LeftButton == ButtonState.Released)
+                    {
+                        _moveAngle = _rotateAngle;
+                        _currentGameState = GameState.Shoot;
+
+                        _PlusY = 3*(float)Math.Sin((_moveAngle));
+                        _PlusX = 3*(float)Math.Cos((_moveAngle));
+
+                    }
+
+                        break;
+
+
+
+                case GameState.Shoot:
+                    _mouseState = Mouse.GetState();
+
+
+                    //_move.Y += 1;
+                    //_move.X += 1;
+      
+
+
+                    _tick += gameTime.ElapsedGameTime.Ticks / (float)TimeSpan.TicksPerSecond;
+                    if (_tick >= 1 / _moveSpeed)
+                    {
+                        _tick = 0;
+                        
+                        _moveY += _PlusX;
+                        _moveX += _PlusY;
+
+                      
+                 
+
+
+                    }
+
+
+
+                    //Trident Move
+                    relPoint = new Vector2((_mouseState.X - TridentPos.X), (_mouseState.Y - TridentPos.Y));
+                    _rotateAngle = (MathHelper.ToDegrees(MathF.Atan2(relPoint.Y, relPoint.X)) + 450f) % 360f;
+                    _rotateAngle = MathHelper.ToRadians(_rotateAngle);
+                    break;
+                case GameState.GameEnded:
+                    break;
+
+            }
             
 
             base.Update(gameTime);
@@ -111,7 +205,8 @@ namespace Bubble
                         }
                         else
                         {
-                            _spriteBatch.Draw(_circle, new Vector2(300 + (j * 60) +15, 5 + (i * 60)), allColor[_bbPos[i, j]]);
+
+                            _spriteBatch.Draw(_circle, new Vector2(300 + (j * 60) + 15, 5 + (i * 60)), allColor[_bbPos[i, j]]); ;
                             _spriteBatch.Draw(_bubble, new Vector2(300 + (j * 60) +15, 5 + (i * 60)), Color.White);
                         }
                     }
@@ -119,25 +214,40 @@ namespace Bubble
             }
 
 
-            
 
-            //Trident
-            if ((_mouseState.X - TridentPos.X) != 0 && _mouseState.X > TridentPos.X)
+            //Trident Drawing
+            _spriteBatch.Draw(_Trident, new Vector2(TridentPos.X, TridentPos.Y), null, Color.White, _rotateAngle, new Vector2(0 + 80, 300), new Vector2(0.6f, 0.6f), SpriteEffects.None, 0f);
+
+            switch (_currentGameState)
             {
-                _rotateAngle = -MathHelper.Pi / 2 + (float)Math.Atan((float)(TridentPos.Y - _mouseState.Y) / (float)(_mouseState.X - TridentPos.X));
-            }
-            else if ((_mouseState.X - TridentPos.X) != 0 && _mouseState.X < TridentPos.X)
-            {
-                _rotateAngle = -MathHelper.Pi / 2 + MathHelper.Pi - (float)Math.Atan((float)(TridentPos.Y - _mouseState.Y) / (float)(TridentPos.X - _mouseState.X));
+                case GameState.GameStarted:
+                    break;
+                case GameState.GamePaused:
+                    break;
+                case GameState.WaitingForShooting:
+                    
+                    //NewBubble Drawing
+                    _spriteBatch.Draw(_circle, new Vector2(TridentPos.X, TridentPos.Y), null, allColor[rnd.Next(allColor.Length)], _rotateAngle, new Vector2(30, 240), 1f, SpriteEffects.None, 0f);
+                    _spriteBatch.Draw(_bubble, new Vector2(TridentPos.X, TridentPos.Y), null, Color.White, _rotateAngle, new Vector2(30, 240), 1f, SpriteEffects.None, 0f);
+
+
+
+                    break;
+                case GameState.Shoot:
+                    //NewBubble Drawing
+                    
+
+
+                    _spriteBatch.Draw(_circle, new Vector2(TridentPos.X + _moveX, TridentPos.Y - _moveY), null, allColor[rnd.Next(allColor.Length)], _moveAngle, new Vector2(30, 240), 1f, SpriteEffects.None, 0f);
+                    _spriteBatch.Draw(_bubble, new Vector2(TridentPos.X, TridentPos.Y), null, Color.White, _rotateAngle, new Vector2(30, 240), 1f, SpriteEffects.None, 0f);
+
+                    break;
+                case GameState.GameEnded:
+                    break;
 
             }
-            else
-            {
-                _rotateAngle = 0;
-            }
+            _spriteBatch.DrawString(_font, "Score : " + MathHelper.ToDegrees(_rotateAngle) + " " + _CurrentBubblePos.X + " " + _moveY, new Vector2(10, 40), Color.White);
 
-            _spriteBatch.Draw(_Trident, new Vector2(TridentPos.X, TridentPos.Y), null, Color.White, -_rotateAngle, new Vector2(0 + 80, 300), 1f, SpriteEffects.None, 0f);
-            
 
 
 
@@ -145,6 +255,34 @@ namespace Bubble
             _graphics.BeginDraw();
 
             base.Draw(gameTime);
+        }
+
+
+        protected void Reset()
+        {
+            _moveSpeed = 300f;
+        }
+
+        protected float[] naive(int x2, int y2)
+        {
+
+            int x1 = 600;
+            int y1 = 880;
+            float[] Plus = new float[2];
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+
+            float m = dy / dx;
+
+            if (dx < 0)
+            {
+                Plus[0] = 1;
+            }else Plus[0] = -1;
+
+            Plus[1] = Math.Abs(m);
+
+
+            return Plus;
         }
     }
 }
